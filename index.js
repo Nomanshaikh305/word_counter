c9c5463e06c99f6f47f29258de5435ea51f798ca
Nomanshaikh305 <160394669+Nomanshaikh305@users.noreply.github.com>
2024-04-19 03:11:58 +0500

    push my word counter to github

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.green.bold("\n \t Welcome to Word Counter Application\n"));
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your Sentence to count the words"
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Your Sentence word count are ${chalk.blue.bold(words.length)}`);
